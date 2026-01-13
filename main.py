b333343ffe585adeecb79a0a3cf6662693e4ccea
Madhura Raut <madhura2137@gmail.com>
2026-01-13 11:03:28 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# main.py
+# Simple Python program for GitHub practice
+
+def greet(name):
+    return f"Hello, {name}! Welcome to your first GitHub project."
+
+def add_numbers(a, b):
+    return a + b
+
+if __name__ == "__main__":
+    print(greet("Madhura"))
+    print("Sum of 10 and 5 is:", add_numbers(10, 5))
